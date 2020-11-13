c246504f90012536145d55e190f6245fa7d2ce16
MikeGattis <mgattis1130@gmail.com>
2020-11-12 19:33:58 -0500

    Database set up with tables Users and Songs

---
+++ migrations/create-users.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user: {
+        type: Sequelize.STRING
+      },
+      user_image_url: {
+        type: Sequelize.STRING
+      },
+      like: {
+        type: Sequelize.BOOLEAN
+      },
+      repost: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
