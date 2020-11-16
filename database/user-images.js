9cb455ea735633970edd118b696f5f3e9bf4a621
MikeGattis <mgattis1130@gmail.com>
2020-11-16 15:37:09 -0500

    Added image urls to database seeding

---
+++ database/user-images.js
@@ -0,0 +1,18 @@
+let images = [
+'https://fec-tyrell-users.s3.us-east-2.amazonaws.com/0415141129a.jpg',
+'https://fec-tyrell-users.s3.us-east-2.amazonaws.com/1403371734449_1476917691428.png',
+'https://fec-tyrell-users.s3.us-east-2.amazonaws.com/20160909_213319.jpg',
+'https://fec-tyrell-users.s3.us-east-2.amazonaws.com/20160927_113007.jpg',
+'https://fec-tyrell-users.s3.us-east-2.amazonaws.com/20161204_202248_1480900983798.jpg',
+'https://fec-tyrell-users.s3.us-east-2.amazonaws.com/20170520_210618.jpg',
+'https://fec-tyrell-users.s3.us-east-2.amazonaws.com/20170614_164651.png',
+'https://fec-tyrell-users.s3.us-east-2.amazonaws.com/20170626_162907.jpg',
+'https://fec-tyrell-users.s3.us-east-2.amazonaws.com/20170813_151710.jpg',
+'https://fec-tyrell-users.s3.us-east-2.amazonaws.com/20171014_094911.jpg',
+'https://fec-tyrell-users.s3.us-east-2.amazonaws.com/20171020_071656_1508602007361.png',
+'https://fec-tyrell-users.s3.us-east-2.amazonaws.com/28153129_1787532741290933_404125326990376960_n-5a9320dda18d9e003727ca39.jpg',
+'https://fec-tyrell-users.s3.us-east-2.amazonaws.com/35123970.jpg',
+'https://fec-tyrell-users.s3.us-east-2.amazonaws.com/4779.jpeg'
+];
+
+module.exports = {images};
